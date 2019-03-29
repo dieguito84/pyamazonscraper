dac011ce0ef760ba917c255380bdadfcac4ed538
dieguito84 <diego.mariani84@gmail.com>
2019-03-29 01:08:12 +0100

    initial commit

--- pyamazontracker.py
+++ pyamazontracker.py
@@ -1,1 +1,13 @@
-#
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.amazon.it/Telecamera-Sorveglianza-Bidirezionale-Infrarossi-Compatibile/dp/B00MA06JXU"
+page = requests.get(URL,headers={"User-Agent":"Defined"})
+soup = BeautifulSoup(page.content, "html.parser")
+title = soup.find(id="productTitle").get_text().strip()
+#price = soup.find(id="priceblock_ourprice").get_text()
+price = soup.find(id="priceblock_dealprice").get_text()
+rating = soup.find(class_="a-icon-star").get_text()
+print(title)
+print(price)
+print(rating)
