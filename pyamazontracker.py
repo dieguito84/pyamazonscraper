32ffc344745b5996e95ff3aa167ef3c69023d8a0
dieguito84 <diego.mariani84@gmail.com>
2019-04-06 11:43:48 +0200

    added distinction from price in offer or not

--- pyamazontracker.py
+++ pyamazontracker.py
@@ -1,24 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.amazon.it/dp/B01CPUGGIW"
-#URL = "https://www.amazon.it/gp/product/B01N2AYZBC"
+#URL = "https://www.amazon.it/dp/B01CPUGGIW"
+URL = "https://www.amazon.it/gp/product/B01N2AYZBC"
 
 page = requests.get(URL,headers={"User-Agent":"Defined"})
 soup = BeautifulSoup(page.content, "html.parser")
 
-title = soup.find(id="productTitle").get_text().strip()
+title = soup.find(id="productTitle").get_text().strip()    # titolo
 
 try:
     price = soup.find(id="priceblock_ourprice").get_text()   # prezzo normale
+    offer = False
 except AttributeError:
     price = soup.find(id="priceblock_dealprice").get_text()   # prezzo offerta
+    offer = True
 
 try:
     rating = soup.find(class_="a-icon-star").get_text()   # rating valorizzato (ci sono recensioni)
 except AttributeError:
-    rating = "Non ci sono recensioni"   # rating non valorizzato (non ci sono recensioni)
+    rating = ""   # rating non valorizzato (non ci sono recensioni)
 
-print(title)
-print(price)
-print(rating)
+print(f"Il titolo dell'articolo è: {title}")
+if offer:
+    print(f"Il prezzo dell'articolo è: {price} e si tratta di un'offerta")
+else:
+    print(f"Il prezzo dell'articolo è: {price}")
+if rating == "":
+    print("Non ci sono recensioni")
+else:
+    print(f"La valutazione dell'articolo è: {rating}")
