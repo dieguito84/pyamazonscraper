4816be393f5e73879dddbfdec072fe26bb4b24c1
dieguito84 <diego.mariani84@gmail.com>
2019-06-21 22:54:45 +0200

    initial commit

---
+++ pricetracker_bs.py
@@ -0,0 +1,60 @@
+import requests
+
+from bs import BeautifulSoup
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0"
+
+# test application performances and code needed to do same things using BeautifulSoup
+
+class PriceTracker:
+    '''
+    Amazxon.it price tracker.
+    '''
+
+    def html(self, url, user_agent=USER_AGENT):
+        '''
+        Get HTML code for given URL.
+        '''
+        pass
+    
+    def title(self, html):
+        '''
+        Parser to find product's title.
+        '''
+        pass
+    
+    def price(self, html):
+        '''
+        Parser to find product's price.
+        '''
+        pass
+    
+    def rating(self, html):
+        '''
+        Parser to find product's rating.
+        '''
+        pass
+    
+    def is_deal(self, html):
+        '''
+        Check if the product is a deal.
+        '''
+        pass
+    
+    def deal_expiry_time(self, html):
+        '''
+        Get time remaining for a deal.
+        '''
+        pass
+    
+
+class Product:
+    '''
+    Basic product details.
+    '''
+
+    def ___init___(self, url, title, price, rating, is_deal=False, offer_expiry_time=None):
+        '''
+        Object constructor.
+        '''
+        pass
