e98c439f62659cb3053f994f39da029ead1ca214
dieguito84 <diego.mariani84@gmail.com>
2019-06-22 17:36:23 +0200

    added TODO

--- pricetracker.py
+++ pricetracker.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0"
 
-# test application performances and code needed to do same things using BeautifulSoup
+# TODO: use SQLite to store url to check, product details, users data for notifications
+# TODO: think the entire application with Telegram Bot integration in mind
 
 class PriceTracker:
     '''
