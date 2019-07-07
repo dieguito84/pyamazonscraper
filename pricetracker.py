aa98605df0ebb4de91438fe0a0a265fed9f12361
dieguito84 <diego.mariani84@gmail.com>
2019-07-07 22:31:56 +0200

    added statements to test insert

--- pricetracker.py
+++ pricetracker.py
@@ -217,4 +217,8 @@ obj = Product(url, pt.title(page), pt.price(page), pt.rating(page), pt.is_deal(p
 obj.details()
 
 db = Database("pricetracker.sqlite3")
+
+product = ("dieguito84", "ABCDE12345", obj.url, obj.title, obj.price, obj.rating, obj.is_deal, obj.deal_expiry_time, "2019-07-06", "4")
+db.insert(product)
+
 db.disconnect()
