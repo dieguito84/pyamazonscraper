c2923d7640ab6c5989a862505588cb2386f89b85
dieguito84 <diego.mariani84@gmail.com>
2019-08-27 17:35:14 +0200

    removed TODO

--- pricetracker.py
+++ pricetracker.py
@@ -357,8 +357,6 @@ def main():
     obj = Product(url, pt.asin(url), pt.title(page), pt.price(page), pt.rating(page), pt.last_check(), pt.is_deal(page), pt.deal_expiry_time(page))
     obj.details()
 
-    # TODO: create a function to get price difference from last check (it should go in PriceTracker class)
-
     db = Database("pricetracker.sqlite3", "products")
 
     product = ("dieguito84", obj.asin, obj.url, obj.title, obj.price, obj.rating, obj.last_check, obj.is_deal, obj.deal_expiry_time, "4")
